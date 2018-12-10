62f86756501016ea454784c47e85088836dfe24f
Arsslensoft <arsslens021@gmail.com>
2018-12-10 22:01:58 +0100

    initial

---
+++ run.bat
@@ -0,0 +1,1 @@
+geth --rpc --datadir=devChain  --rpccorsdomain "*" --mine --rpcapi "eth,web3,personal,net,miner,admin,debug" --ws  --wsaddr "0.0.0.0" --wsapi "eth,web3,personal,net,miner,admin,debug" --wsorigins "*" --unlock 0x12890d2cce102216644c59daE5baed380d84830c --password "pass.txt"  --verbosity 0 --networkid 300 console
